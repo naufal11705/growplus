56a713caa90298fed998af90e4c7fc78ae8e8386
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-03-11 11:51:04 +0700

    feat: added the core database structure files

---
+++ app/Http/Controllers/FasKesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FasKes;
+use Illuminate\Http\Request;
+
+class FasKesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FasKes $fasKes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FasKes $fasKes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FasKes $fasKes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FasKes $fasKes)
+    {
+        //
+    }
+}
