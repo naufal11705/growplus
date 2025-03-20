ce3ca0b97fc4f1bf7b157a625f2d83d183099a6e
ckckckcz <rriovld@gmail.com>
2025-03-21 05:03:54 +0700

    feat: add tables for managing articles, phases, healthcare facilities, and challenges with edit and delete actions

--- resources/js/Pages/Admin/Imunisasi.tsx
+++ resources/js/Pages/Admin/Imunisasi.tsx
@@ -9,6 +9,62 @@ export default function Imunisasi(){
                             Tambah Imunisasi
                         </button>
                     </a>
+                    <div className="relative overflow-x-auto border rounded-xl mt-5">
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Nama
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Jenis
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Usia Minimum
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Usia Maksimum
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Puskesmas ID
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Aksi
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="bg-white border-b border-gray-200 hover:bg-gray-50">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        Disini
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        Disini
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        Disini
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        Disini
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        Disini
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        Disini
+                                    </td>
+                                    <td className="px-6 py-4 text-right flex gap-2">
+                                        <a href="#" className="font-medium text-wine hover:underline">Edit</a>
+                                        │
+                                        <a href="#" className="font-medium text-red-500 hover:underline">Delete</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </Layout>
