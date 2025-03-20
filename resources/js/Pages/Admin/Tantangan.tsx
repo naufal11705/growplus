57b3234c72a8ac2c7fba8c4713f809bb56d0eaaf
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2025-03-21 00:21:51 +0700

    refactor: update routes to match resourceful routing

--- resources/js/Pages/Admin/Tantangan.tsx
+++ resources/js/Pages/Admin/Tantangan.tsx
@@ -4,7 +4,7 @@ export default function Tantangan(){
         <Layout>
             <div className="lg:p-8 p-1 sm:ml-64 lg:mt-12 mt-8 md:mt-14">
                 <div className="lg:p-8 p-4">
-                    <a href="/admin/tantangan/tambah">
+                    <a href="/admin/tantangan/create">
                         <button type="button" className="px-5 py-3 text-base font-medium text-center text-white bg-wine rounded-xl hover:bg-dark-wine">
                             Tambah Tantangan
                         </button>
