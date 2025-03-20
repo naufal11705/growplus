852296b01b297f6a6595e5b954a50c94cd8a0962
ckckckcz <rriovld@gmail.com>
2025-03-20 15:24:03 +0700

    feat: enhance login form with Inertia.js handling and validation errors

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,11 +1,23 @@
-import Layout from "../../Layouts/Auth";
+import Layout from "@/Layouts/Auth";
+import { useForm } from "@inertiajs/react";
+import { FormEvent } from "react";
+
 export default function Login() {
+    const { data, setData, post, processing, errors } = useForm({
+        email: "",
+        password: "",
+    });
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        post(route("login"));
+    };
+
     return (
         <Layout>
-            <section className="">
+            <section>
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen min-h-screen lg:py-0">
-                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
-                        {/* <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" /> */}
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
                         Grow+
                     </a>
                     <div className="w-full bg-white rounded-2xl shadow border border-gray-200 md:mt-0 sm:max-w-md xl:p-0">
@@ -13,27 +25,64 @@ export default function Login() {
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                                 Selamat Datang Kembali 👋🏽
                             </h1>
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Masukkan Email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-xl focus:ring-wine focus:border-wine block w-full p-2.5" placeholder="name@company.com" required />
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+                                        Masukkan Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        value={data.email}
+                                        onChange={(e) => setData("email", e.target.value)}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-xl focus:ring-wine focus:border-wine block w-full p-2.5"
+                                        placeholder="name@company.com"
+                                        required
+                                    />
+                                    {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
                                 </div>
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Masukkan Kata Sandi</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-xl focus:ring-wine focus:border-wine block w-full p-2.5" required />
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
+                                        Masukkan Kata Sandi
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        value={data.password}
+                                        onChange={(e) => setData("password", e.target.value)}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-xl focus:ring-wine focus:border-wine block w-full p-2.5"
+                                        placeholder="••••••••"
+                                        required
+                                    />
+                                    {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
                                 </div>
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-start">
                                         <div className="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-light-pinky" required />
+                                            <input
+                                                id="remember"
+                                                aria-describedby="remember"
+                                                type="checkbox"
+                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-light-pinky"
+                                            />
                                         </div>
                                         <div className="ml-3 text-sm">
                                             <label htmlFor="remember" className="text-gray-500">Ingat saya</label>
                                         </div>
                                     </div>
-                                    <a href="/forgotPassword" className="text-sm font-medium text-wine hover:underline">Lupa Kata Sandi?</a>
+                                    <a href="/forgotPassword" className="text-sm font-medium text-wine hover:underline">
+                                        Lupa Kata Sandi?
+                                    </a>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-wine hover:bg-dark-wine focus:ring-4 focus:outline-none focus:ring-light-pinky font-medium rounded-xl text-md px-5 py-3 text-center">Masuk</button>
+                                <button
+                                    type="submit"
+                                    className="w-full text-white bg-wine hover:bg-dark-wine focus:ring-4 focus:outline-none focus:ring-light-pinky font-medium rounded-xl text-md px-5 py-3 text-center"
+                                    disabled={processing}
+                                >
+                                    {processing ? "Memproses..." : "Masuk"}
+                                </button>
                                 <p className="text-sm font-light text-gray-500">
                                     Belum Punya Akun? <a href="/register" className="font-bold text-wine hover:underline">Daftar</a>
                                 </p>
