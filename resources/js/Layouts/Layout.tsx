c65c07e6571ff9c60efd37014f367620e42bd97c
Lavina-23 <lavinakohai3227@gmail.com>
2025-03-23 20:46:52 +0700

    feat: implement sidebar toggle functionality in Header and Layout components

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -1,15 +1,22 @@
-import Sidebar from '../Components/Sidebar';
+import { useState } from "react";
 import Header from '../Components/Header';
+import Sidebar from '../Components/Sidebar';
+
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
         <>
             <div className="bg-white min-h-screen">
-                <Header />  
-                <Sidebar />
+                <Header isOpen={isOpen} toggleSidebar={toggleSidebar} />
+                <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
                 <main>{children}</main>
             </div>
         </>
