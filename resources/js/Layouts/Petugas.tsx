c56115379233b66ca77930f704bb014ca559ef6e
Lavina-23 <lavinakohai3227@gmail.com>
2025-03-23 23:53:51 +0700

    feat: implement sidebar toggle functionality in Admin and Petugas layouts

--- resources/js/Layouts/Petugas.tsx
+++ resources/js/Layouts/Petugas.tsx
@@ -1,16 +1,22 @@
-import Sidebar from '@/Components/Petugas/Sidebar';
 import Header from '@/Components/Petugas/Header';
+import Sidebar from '@/Components/Petugas/Sidebar';
+import { useState } from 'react';
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
         <>
             <div className="bg-white min-h-screen">
-                <Header />  
-                <Sidebar />
-                <main>{children}</main>
+                <Header isOpen={isOpen} toggleSidebar={toggleSidebar} />
+                <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+                <main className="mt-16 md:mt-0">{children}</main>
             </div>
         </>
     );
