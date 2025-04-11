f8afbb959ab71da2acd62ff600f0f554f7175b2c
Lavina-23 <lavinakohai3227@gmail.com>
2025-04-12 06:47:33 +0700

    fix: update streak calculation to count AnakTantangan records in dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
 
             foreach ($anakIds as $anakId) {
                 $totalPoints += $this->anakTantanganRepository->countTotalPoints($anakId);
-                $streakForThisAnak = $this->anakTantanganRepository->getAnakTantangansByAnakId($anakId);
+                $streakForThisAnak = $this->anakTantanganRepository->getAnakTantangansByAnakId($anakId)->count();
                 $totalStreak += $streakForThisAnak;
             }
 
