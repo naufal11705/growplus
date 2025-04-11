ea7b8541815f98a731d9295ba621407cf6c0ca94
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2025-04-11 16:50:31 +0700

    feat: add validation rules for jenis_kelamin

--- app/Http/Requests/AnakUpdateRequest.php
+++ app/Http/Requests/AnakUpdateRequest.php
@@ -14,7 +14,16 @@ class AnakUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'orangtua_id' => ['required', 'integer', 'exists:orang_tuas,orangtua_id'],
+            'nama' => ['required', 'string', 'max:255'],
+            'nik' => ['required', 'string', 'min:16'],
+            'no_jkn' => ['required', 'string', 'min:13'],
+            'tempat_lahir' => ['required', 'string', 'max:255'],
+            'tanggal_lahir' => ['required', 'date'],
+            'golongan_darah' => ['required', 'string', 'max:3'],
+            'berat_badan' => ['required', 'integer'],
+            'tinggi_badan' => ['required', 'integer'],
+            'jenis_kelamin' => ['required', 'string', 'max:1'],
         ];
     }
 }
